1ffeba622bb88fbe15e86ae301140447f766cc9a
kidon-fedora-local <kidon@sterilngtrader.com>
2014-03-18 00:09:22 -0500

    Fix jQuery undefined error by merging jquery with app js

--- contentScript.js
+++ contentScript.js
@@ -4,5 +4,4 @@ function injectJs(srcFile) {
 	document.getElementsByTagName('head')[0].appendChild(scr);
 }
 
-injectJs(chrome.extension.getURL('js/jquery.js'));
 injectJs(chrome.extension.getURL('js/rtmsfdt.js'));
